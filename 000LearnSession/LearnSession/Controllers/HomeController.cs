c6961ae5cf2cef996585fff1cefe17e607e3dfe1
sfontanach <fontana.stefano0@gmail.com>
2017-09-05 12:38:52 +0200

    Add test project about sessions.
    Create a cookie if it doesn't exist and keep adding a value at
    each visit.

---
+++ 000LearnSession/LearnSession/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string SessionKey = "Key";
+
+        public IActionResult Index()
+        {
+            var value = (HttpContext.Session.GetInt32(SessionKey) ?? 0) + 1;
+            HttpContext.Session.SetInt32(SessionKey, value);
+            return Content($"This is your {value}. visit");
+        }
+    }
+}
