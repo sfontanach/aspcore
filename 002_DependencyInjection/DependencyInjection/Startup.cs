81fa08f96020b3162a98e0f04552636c836eba5f
sfontanach <fontana.stefano0@gmail.com>
2017-09-09 16:06:35 +0200

    Add some other tests.

---
+++ 002_DependencyInjection/DependencyInjection/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DI_Example
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//services.AddTransient<IUserService, UserService>();
+			//services.AddTransient<IUserService, FakeUserService>();
+			//services.AddTransient<UserContext, UserContext>(); //class class mapping is allowed
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseDeveloperExceptionPage();
+			app.UseMiddleware<UserMiddleware>();
+		}
+	}
+
+
+	public class UserMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly IUserService _userService;
+
+		public UserMiddleware(RequestDelegate next, IUserService userService) // Captive Dependency 
+		{
+			_next = next;
+			_userService = userService;
+		}
+
+
+		public async Task Invoke(HttpContext context) // Captive Dependency 
+		{
+			await context.Response.WriteAsync(_userService.Users.Any() ? string.Join(", ", _userService.Users) : "None");
+		}
+
+		/*
+        public async Task Invoke(HttpContext context,  IEnumerable<IUserService> services) // no Captive Dependency 
+        {
+            foreach (var service in services)
+            {
+                await context.Response.WriteAsync(service.GetType().Name + "\t\t\t" + (service.Users.Any() ? string.Join(", ", service.Users) : "None") + Environment.NewLine);
+            }
+        }
+        */
+
+	}
+
+
+	public interface IUserService
+	{
+		string[] Users { get; }
+	}
+
+	public class UserService : IUserService
+	{
+		public string[] Users { get; } = { };
+	}
+
+	public class FakeUserService : IUserService
+	{
+		public string[] Users { get; } = { "Michael", "Bob" };
+	}
+
+	/*
+    public class UserService : IUserService
+    {
+        private readonly UserContext _userContext;
+
+        public UserService(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public string[] Users => _userContext.LoadFromDataBase();
+    }
+    */
+	public class UserContext
+	{
+		public string[] LoadFromDataBase()
+		{
+			return new[] { "Lisa", "Yve" };
+		}
+	}
+}
---
+++ 003_Middleware/Middleware/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MiddlewareDemo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Handling request: " + context.Request.Path);
+                await next.Invoke();
+            });
+
+
+            app.Map("/hallo", builder => builder.UseWelcomePage());
+            app.UseWelcomePage("/hallo");
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World! 2");
+            });
+
+       
+            app.Map("/logging", builder =>
+            {
+                builder.UseMiddleware<RequestLoggerMiddleware>();
+                
+                builder.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            app.UseWelcomePage();
+            app.UseMiddleware<RequestLoggerMiddleware>();
+
+
+            app.Map("/logging", builder =>
+            {
+                builder.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+
+    }
+
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("Handling request: " + context.Request.Path);
+            await context.Response.WriteAsync("You have been watched!");
+            //await _next.Invoke(context);
+            System.Diagnostics.Debug.WriteLine("Finished handling request.");
+        }
+    }
+
+
+}
