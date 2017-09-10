ae3bf35b82f29dacc0371bda2ff1e6178365c629
sfontanach <fontana.stefano0@gmail.com>
2017-09-10 13:18:24 +0200

    [004] Add search.

---
+++ 004_MvcMovie/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public string movieGenre { get; set; }
+
+    }
+}
